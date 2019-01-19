c47b1e83b12323d27041a1ac24fca3536b05bccd
coco11111 <wangzhang.love@gmail.com>
2019-01-19 15:02:54 -0500

    Update plot1.R

--- plot1.R
+++ plot1.R
@@ -4,9 +4,6 @@ Sys.setlocale("LC_TIME", "English")
 library(sqldf)
 data <- read.csv.sql("household_power_consumption.txt", sql = "select * from file where Date ='1/2/2007' or Date ='2/2/2007'", header = TRUE, sep = ";")
 
-
-#data$Global_active_power <- as.numeric(data$Global_active_power)
-
 #plot1
 png("plot1.png", width = 480, height = 480)
 with(data,hist(Global_active_power,main = "Global Active Power",xlim = c(0,6),ylim = c(0,1200),col = "red",xlab = "Global Active Power (kilowatts)"))
