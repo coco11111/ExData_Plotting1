9eeab4ddbe139c9dc3b2a5458a1e399d570d6afb
coco11111 <wangzhang.love@gmail.com>
2019-01-19 15:08:33 -0500

    Update plot4.R

--- plot4.R
+++ plot4.R
@@ -2,10 +2,10 @@ setwd("E:/r/4.exploratory")
 Sys.setlocale("LC_TIME", "English")
 #read the data.The data set is large and we only read the data from the dates 2007-02-01 and 2007-02-02 only. 
 library(sqldf)
-data <- read.csv.sql("household_power_consumption.txt", sql = "select * from file where Date ='2/1/2007' or Date ='2/2/2007'", header = TRUE, sep = ";")
+data <- read.csv.sql("household_power_consumption.txt", sql = "select * from file where Date ='1/2/2007' or Date ='2/2/2007'", header = TRUE, sep = ";")
 
 x <-paste(data$Date,data$Time)
-y <- strptime(x, "%m/%d/%Y %H:%M:%S")
+y <- strptime(x, "%d/%m/%Y %H:%M:%S")
 
 png("plot4.png", width = 480, height = 480)
 par(mfrow=c(2,2))
